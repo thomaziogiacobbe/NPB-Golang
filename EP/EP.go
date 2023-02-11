f582e6abfddf17f672d9cdad28881683e47c156a
thomazio <tgiacobbe@outlook.com>
2023-02-11 17:58:51 -0300

    added most of the code before the parallel execution

--- EP/EP.go
+++ EP/EP.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	npb "NPB-Golang/commons"
 	"fmt"
 	"math"
 	"os"
@@ -19,8 +20,8 @@ func main() {
 
 	const MK = int(16)
 
-	//var MM = M - MK
-	//var NN = 1 << MM
+	var MM = M - MK
+	var NN = 1 << MM
 
 	const NK = 1 << MK
 	const NQ = 10
@@ -30,17 +31,53 @@ func main() {
 	const NK_PLUS = (2 * NK) + 1
 
 	var size float64
-	//var mops, t1, t2, t3, t4, x1, x2 float64
-	//var sx, sy, tm, an, tt, gc float64
-	//var sx_verify_value, sy_verify_value, sx_err, sy_err float64
-	//var np int32
-	//var i, ik, kk, l, k, nit int32
-	//var k_offset, j int32
-	//var verified bool
-	//dum := [3]float64{1.0, 1.0, 1.0}
+	var mops, t1, t2, t3, t4, x1, x2 float64
+	var sx, sy, tm, an, tt, gc float64
+	var sx_verify_value, sy_verify_value, sx_err, sy_err float64
+	var np int32
+	var ik, kk, l, k, nit int32
+	var k_offset, j int32
+	var verified bool
+	dum := [3]float64{1.0, 1.0, 1.0}
 	size = math.Pow(2.0, float64(M+1))
+	var x = [NK_PLUS]float64{}
+	var q = [NQ]float64{}
 
-	fmt.Println(size)
+	fmt.Println("\n\n NAS Parallel Benchmarks 4.1 Parallel Golang version - EP Benchmark\\n\\n")
+	fmt.Println(" Number of random numbers generated:", size)
 
-	fmt.Println(verify(1, 1))
+	verified = false
+
+	np = NN
+
+	temp := []float64{dum[2]}
+	npb.Vranlc(0, &dum[0], dum[1], temp)
+	dum[0] = npb.Randlc(&dum[1], dum[2])
+	for i := 0; i < NK_PLUS; i++ {
+		x[i] = -1.0e99
+	}
+	mops = math.Log(math.Sqrt(math.Abs(math.Max(1.0, 1.0))))
+
+	//TODO: add timers
+
+	t1 = A
+	npb.Vranlc(0, &t1, A, x[:])
+
+	t1 = A
+
+	for i := 0; i < MK+1; i++ {
+		t2 = npb.Randlc(&t1, t1)
+	}
+
+	an = t1
+	tt = S
+	gc = 0.0
+	sx = 0.0
+	sy = 0.0
+
+	for i := 0; i <= NQ-1; i++ {
+		q[i] = 0.0
+	}
+
+	k_offset = -1
 }
