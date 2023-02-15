00c3a6f545dc208e364d490b62e5208a664bdeb9
thomazio <tgiacobbe@outlook.com>
2023-02-15 10:12:11 -0300

    cleaned unused variables

--- EP/EP.go
+++ EP/EP.go
@@ -17,7 +17,6 @@ const (
 	MK      = int(16)
 	NK      = 1 << MK
 	NQ      = 10
-	EPSILON = 1.0e-8
 	A       = 1220703125.0
 	S       = 271828183.0
 	NK_PLUS = (2 * NK) + 1
@@ -36,11 +35,11 @@ func main() {
 	NN = 1 << MM
 
 	var size float64
-	var mops, t1, t2, t3, t4, x1, x2 float64
-	var sx, sy, tm, an, tt, gc float64
+	var mops, t1 float64
+	//TODO: variable tt is for timer
+	var sx, sy, tm, an, gc /*, tt*/ float64
 	var np int
-	var ik, kk, l, k, nit int32
-	var j int32
+	var nit int32
 	var verified bool
 	dum := [3]float64{1.0, 1.0, 1.0}
 	size = math.Pow(2.0, float64(M+1))
@@ -70,11 +69,12 @@ func main() {
 	t1 = A
 
 	for i := 0; i < MK+1; i++ {
-		t2 = npb.Randlc(&t1, t1)
+		npb.Randlc(&t1, t1)
 	}
 
 	an = t1
-	tt = S
+	//TODO: variable tt is for timer
+	//tt = S
 	gc = 0.0
 	sx = 0.0
 	sy = 0.0
