0df2ba1c5c3fb39302267e69c647834dbfb4f8b8
KDOXG <kevin.20pereira@gmail.com>
2023-03-07 19:11:16 -0300

    fix var name max key

--- IS/IS.go
+++ IS/IS.go
@@ -5,7 +5,7 @@ var (
 	max_key_log_2     int
 	num_buckets_log_2 int
 	total_keys        int
-	max_keys          int
+	max_key           int
 	num_buckets       int
 	num_keys          int
 	size_of_buffers   int
@@ -22,4 +22,10 @@ func ExecIS() {
 	//TODO: Initialize the verification arrays if a valid class
 	//TODO: IS print
 
+	//TODO: create_seq
+	/* Generate random number sequence and subsequent keys on all procs */
+	/* ... */
+	/* Wait, does the variables keep existing after the parallel block's end? */
+	//create_seq(314159265.00 /* Random number gen seed */,
+	//	1220703125.00 /* Random number gen mult */);
 }
