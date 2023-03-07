e1d3da195521cb4bb0449a16ced4387a2ca106d1
thomazio <tgiacobbe@outlook.com>
2023-03-07 19:02:15 -0300

    created variables

--- IS/IS.go
+++ IS/IS.go
@@ -1,5 +1,18 @@
 package IS
 
+var (
+	total_keys_log    int
+	max_key_log       int
+	num_buckets_log_2 int
+	total_keys        int
+	max_keys          int
+	num_buckets       int
+	num_keys          int
+	size_of_buffers   int
+	max_iterations    int
+	test_array_size   int
+)
+
 func ExecIS() {
 
 }
