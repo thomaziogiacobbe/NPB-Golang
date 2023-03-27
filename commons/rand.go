b6c052e7e37ca7be75340519869d882088da5823
thomazio <tgiacobbe@outlook.com>
2023-03-26 23:04:31 -0300

    rand functions now use formula to generate numbers still experimental

--- commons/rand.go
+++ commons/rand.go
@@ -1,5 +1,7 @@
 package commons
 
+import "math"
+
 const (
 	r23 = 0.5 * 0.5 * 0.5 * 0.5 * 0.5 * 0.5 * 0.5 * 0.5 * 0.5 * 0.5 * 0.5 * 0.5 * 0.5 * 0.5 * 0.5 * 0.5 * 0.5 * 0.5 * 0.5 * 0.5 * 0.5 * 0.5 * 0.5
 	t23 = 2.0 * 2.0 * 2.0 * 2.0 * 2.0 * 2.0 * 2.0 * 2.0 * 2.0 * 2.0 * 2.0 * 2.0 * 2.0 * 2.0 * 2.0 * 2.0 * 2.0 * 2.0 * 2.0 * 2.0 * 2.0 * 2.0 * 2.0
@@ -7,46 +9,34 @@ const (
 	t46 = t23 * t23
 )
 
+// TODO: verificar o pq essa funcao fica mais lenta usando a formula
 func Vranlc(n int, xSeed *float64, a float64, y []float64) {
 
-	var x, t1, t2, t3, t4, a1, a2, x1, x2, z float64
+	var x float64
 
-	t1 = r23 * a
-	a1 = float64(int(t1))
-	a2 = a - t23*a1
 	x = *xSeed
-
 	for i := 0; i < n; i++ {
-		t1 = r23 * x
-		x1 = float64(int(t1))
-		x2 = x - t23*x1
-		t1 = a1*x2 + a2*x1
-		t2 = float64(int(r23 * t1))
-		z = t1 - t23*t2
-		t3 = t23*z + a2*x2
-		t4 = float64(int(r46 * t3))
-		x = t3 - t46*t4
-		y[i] = r46 * x
+		ux := uint64(x)
+		ua := uint64(a)
+		mul := ux * ua
+		x = float64(mul % uint64(t46))
+		y[i] = math.Pow(2, -46) * (x)
 	}
 	*xSeed = x
 }
 
+// TODO: verificar se para outras classes usar a formula pode causar problema
 func Randlc(x *float64, a float64) float64 {
-	var t1, t2, t3, t4, a1, a2, x1, x2, z float64
-
-	t1 = r23 * a
-	a1 = float64(int(t1))
-	a2 = a - t23*a1
-
-	t1 = r23 * (*x)
-	x1 = float64(int(t1))
-	x2 = (*x) - t23*x1
-	t1 = a1*x2 + a2*x1
-	t2 = float64(int(r23 * t1))
-	z = t1 - t23*t2
-	t3 = t23*z + a2*x2
-	t4 = float64(int(r46 * t3))
-	*x = t3 - t46*t4
-
-	return r46 * (*x)
+	ux := uint64(*x)
+	ua := uint64(a)
+	mul := ux * ua
+	*x = float64(mul % uint64(t46))
+	ret := math.Pow(2, -46) * (*x)
+	return ret
 }
+
+/* formula original do jeito mais simples
+   causa problemas nas operacoes com float
+*x = math.Mod(a*(*x), t46)
+return math.Pow(2, -46) * (*x)
+*/
