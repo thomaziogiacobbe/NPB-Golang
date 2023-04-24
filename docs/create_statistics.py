959614d747b41c57a50bf70e96d03b8b8c76d929
KDOXG <kevin.20pereira@gmail.com>
2023-04-24 20:02:29 -0300

    updated statistics script, commented out execution lines

--- docs/create_statistics.py
+++ docs/create_statistics.py
@@ -6,15 +6,15 @@ def create_statistics(filename):
 	times = [float(x) for x in f.readlines()]
 	return dict(filename = filename, mean = st.mean(times), stdev = st.stdev(times), time = times)
 
-a = create_statistics("ep.a.txt")
-b = create_statistics("ep.b.txt")
-s = create_statistics("ep.s.txt")
-serial_a = create_statistics("ep_serial.a.txt")
-serial_s = create_statistics("ep_serial.s.txt")
-dalvan_serial_a = create_statistics("ep_serial.A.cpp.txt")
-dalvan_serial_s = create_statistics("ep_serial.S.cpp.txt")
-newa = create_statistics("ep_a.txt")
-news = create_statistics("ep_s.txt")
+#a = create_statistics("ep.a.txt")
+#b = create_statistics("ep.b.txt")
+#s = create_statistics("ep.s.txt")
+#serial_a = create_statistics("ep_serial.a.txt")
+#serial_s = create_statistics("ep_serial.s.txt")
+#dalvan_serial_a = create_statistics("ep_serial.A.cpp.txt")
+#dalvan_serial_s = create_statistics("ep_serial.S.cpp.txt")
+#newa = create_statistics("ep_a.txt")
+#news = create_statistics("ep_s.txt")
 
 #mean
 #points = [a["mean"], b["mean"], s["mean"], serial_a["mean"], serial_s["mean"], dalvan_serial_a["mean"], dalvan_serial_s["mean"]]
